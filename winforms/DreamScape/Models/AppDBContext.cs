a42abe0c443a53ff297b5ec68a5dceea893a4dfb
Wouter Smaardijk <287574@edu.rocwb.nl>
2023-09-15 18:27:46 +0200

    seeder update

--- winforms/DreamScape/Models/AppDBContext.cs
+++ winforms/DreamScape/Models/AppDBContext.cs
@@ -61,12 +61,12 @@ namespace DreamScape.Models
             // Seed data for items
             modelBuilder.Entity<Item>().HasData(
         new Item { Id = 1, Name = "Vlammenzwaard", Description = "Een zwaard doordrenkt met vlammende energie.", Type = ItemType.Weapon, Rarity = 85, Power = 90, Speed = 70, Durability = 80, Magic = 60 },
-        new Item { Id = 2, Name = "Schaduwkap", Description = "Een mysterieuze kap die de drager in duisternis hult.", Type = ItemType.Armor, Rarity = 75, Power = 20, Speed = 40, Durability = 90, Magic = 50 },
-        new Item { Id = 3, Name = "Amulet van Genezing", Description = "Een amulet dat helende krachten bezit.", Type = ItemType.Accessory, Rarity = 60, Power = 10, Speed = 50, Durability = 70, Magic = 90 },
+        new Item { Id = 2, Name = "Schaduwkap", Description = "Een mysterieuze kap die de drager in duisternis hult.", Type = ItemType.Armor, Rarity = 40, Power = 20, Speed = 40, Durability = 90, Magic = 50 },
+        new Item { Id = 3, Name = "Amulet van Genezing", Description = "Een amulet dat helende krachten bezit.", Type = ItemType.Accessory, Rarity = 15, Power = 10, Speed = 50, Durability = 70, Magic = 90 },
         new Item { Id = 4, Name = "Bliksempistool", Description = "Een vuurwapen dat dodelijke bliksemprojectielen afvuurt.", Type = ItemType.Weapon, Rarity = 80, Power = 70, Speed = 90, Durability = 60, Magic = 30 },
-        new Item { Id = 5, Name = "IJzeren Harnas", Description = "Een stevig harnas dat bescherming biedt tegen fysieke aanvallen.", Type = ItemType.Armor, Rarity = 70, Power = 30, Speed = 20, Durability = 100, Magic = 0 },
+        new Item { Id = 5, Name = "IJzeren Harnas", Description = "Een stevig harnas dat bescherming biedt tegen fysieke aanvallen.", Type = ItemType.Armor, Rarity = 30, Power = 30, Speed = 20, Durability = 100, Magic = 0 },
         new Item { Id = 6, Name = "Ring van Onzichtbaarheid", Description = "Een ring die de drager onzichtbaar maakt voor vijanden.", Type = ItemType.Accessory, Rarity = 90, Power = 0, Speed = 80, Durability = 50, Magic = 50 },
-        new Item { Id = 7, Name = "Toverstaf van Verwarring", Description = "Een toverstaf die vijanden in verwarring brengt.", Type = ItemType.Weapon, Rarity = 75, Power = 60, Speed = 60, Durability = 50, Magic = 80 },
+        new Item { Id = 7, Name = "Toverstaf van Verwarring", Description = "Een toverstaf die vijanden in verwarring brengt.", Type = ItemType.Weapon, Rarity = 50, Power = 60, Speed = 60, Durability = 50, Magic = 80 },
         new Item { Id = 8, Name = "Draaksschild", Description = "Een schild gemaakt van drakenschubben dat vuuraanvallen absorbeert.", Type = ItemType.Armor, Rarity = 85, Power = 20, Speed = 30, Durability = 90, Magic = 40 },
         new Item { Id = 9, Name = "Amulet van Levitatie", Description = "Een amulet waarmee de drager kan zweven.", Type = ItemType.Accessory, Rarity = 60, Power = 10, Speed = 70, Durability = 50, Magic = 70 },
         new Item { Id = 10, Name = "Zwaard van Licht", Description = "Een zwaard dat stralend licht uitstraalt en duistere wezens schade toebrengt.", Type = ItemType.Weapon, Rarity = 95, Power = 80, Speed = 50, Durability = 70, Magic = 40 },
@@ -78,7 +78,7 @@ namespace DreamScape.Models
         new Item { Id = 16, Name = "Boog van de Wind", Description = "Een boog die pijlen afvuurt met de kracht van de wind.", Type = ItemType.Weapon, Rarity = 75, Power = 40, Speed = 80, Durability = 50, Magic = 50 },
         new Item { Id = 17, Name = "Schubbenharnas", Description = "Een harnas gemaakt van drakenschubben dat fysieke aanvallen vermindert.", Type = ItemType.Armor, Rarity = 70, Power = 10, Speed = 10, Durability = 100, Magic = 0 },
         new Item { Id = 18, Name = "Amulet van Wijsheid", Description = "Een amulet dat de wijsheid en kennis van de drager vergroot.", Type = ItemType.Accessory, Rarity = 90, Power = 0, Speed = 40, Durability = 60, Magic = 90 },
-        new Item { Id = 19, Name = "Vuurzwaard", Description = "Een zwaard dat brandende vuurprojectielen afvuurt.", Type = ItemType.Weapon, Rarity = 80, Power = 70, Speed = 50, Durability = 60, Magic = 30 },
+        new Item { Id = 19, Name = "Vuurzwaard", Description = "Een zwaard dat brandende vuurprojectielen afvuurt.", Type = ItemType.Weapon, Rarity = 50, Power = 70, Speed = 50, Durability = 60, Magic = 30 },
         new Item { Id = 20, Name = "Betoverde Laarzen", Description = "Laarzen die de snelheid en behendigheid van de drager verhogen.", Type = ItemType.Armor, Rarity = 75, Power = 20, Speed = 90, Durability = 60, Magic = 10 }
     );
         }
